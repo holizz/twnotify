49155e016284678426a0e4f0f56e62a943aab6bf
Tom Adams <tom@holizz.com>
2009-04-24 15:42:24 +0100

    Use profile icons.

--- twnotify.py
+++ twnotify.py
@@ -21,8 +21,14 @@ import sys; reload(sys); sys.setdefaultencoding('utf-8')
 import feedparser, urllib, os, re, subprocess, sys, time, yaml
 
 class TwNotify:
-    def __init__(self):
+    def __init__(self, icons=True):
         self.seen = {}
+        self.icons = icons
+        self.authfile = os.environ['HOME']+'/.twitterauth'
+        if icons:
+            self.icondir = os.environ['HOME']+'/.cache/twnotify'
+            if not os.path.isdir(self.icondir):
+                os.makedirs(self.icondir)
         self.checkfeed(notify=False)
 
     def run(self, timeout=300):
@@ -31,7 +37,7 @@ class TwNotify:
             self.checkfeed()
 
     def checkfeed(self, notify=True):
-        auth = yaml.load(open(os.environ['HOME']+'/.twitterauth'))
+        auth = yaml.load(open(self.authfile))
         file = urllib.urlopen('https://%s:%s@twitter.com/statuses/friends_timeline.atom' % (auth['Username'], auth['Password']))
         del auth # try not to leave passwords in memory for too long
         feed = feedparser.parse(file)
@@ -39,11 +45,29 @@ class TwNotify:
         for entry in reversed(feed.entries):
             if notify and not self.seen.has_key(entry.id):
                 author, msg = re.match('^(.*?): (.*)$', entry.title).group(1,2)
-                self.notify(author, msg)
-            self.seen[entry.id] = True
+                icon = None
+                if self.icons:
+                    iconurl = entry.links[1].href
+                    uid, file = re.match('.*/(\d+)/(.+)$', iconurl).group(1,2)
+                    iconuid = self.icondir+'/'+uid
+                    iconfile = self.icondir+'/'+uid+'/'+file
+                    if not os.path.isfile(iconfile):
+                        if not os.path.isdir(iconuid):
+                            os.makedirs(iconuid)
+                        with open(iconfile,'w+') as fo:
+                            fo.write(urllib.urlopen(iconurl).read())
+                    icon = iconfile
 
-    def notify(self, title, body):
-        if subprocess.call(['notify-send', title, body]) != 0:
+                self.notify(author, msg, icon=icon)
+            self.seen[entry.id] = True
+        del feed # passwords appear here too!
+
+    def notify(self, title, body, icon=None):
+        sub = ['notify-send', title, body]
+        if icon:
+            sub.append('-i')
+            sub.append(icon)
+        if subprocess.call(sub) != 0:
             raise Exception("didn't work")
 
 if __name__ == '__main__':
